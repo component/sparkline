84c39c5458f2bf537536b43abc23ff9c5f44a76d
TJ Holowaychuk <tj@vision-media.ca>
2012-11-03 00:48:50 -0700

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,24 @@
+
+# sparkline
+
+  Tiny spark line graphs.
+
+## Installation
+
+    $ component install component/sparkline
+
+## API
+
+### Sparkline(canvas)
+
+  Initialize with the given `canvas`.
+
+### Sparkline#update(data)
+
+  Update `data` points, a series of floats. The sparkline
+  will be drawn and automatically compressed to fit within
+  the `canvas` dimensions.
+
+## License
+
+  MIT
---
+++ index.js
@@ -0,0 +1,51 @@
+
+/**
+ * Module dependencies.
+ */
+
+var max = require('max');
+
+/**
+ * Expose `Sparkline`.
+ */
+
+module.exports = Sparkline;
+
+/**
+ * Initialize a sparkline with `canvas`.
+ *
+ * @param {Canvas} canvas
+ * @api public
+ */
+
+function Sparkline(canvas) {
+  if (!canvas) throw new Error('sparkline canvas required');
+  this.canvas = canvas;
+  this.ctx = canvas.getContext('2d');
+}
+
+/**
+ * Update `data`.
+ *
+ * @param {Array} data
+ * @api public
+ */
+
+Sparkline.prototype.update = function(data){
+  var canvas = this.canvas;
+  var ctx = this.ctx;
+  var len = data.length;
+  var w = canvas.width;
+  var h = canvas.height;
+  var m = max(data);
+  var sx = w / len;
+  var x = 0;
+
+  canvas.width = w;
+  ctx.beginPath();
+  for (var i = 0; i < len; ++i) {
+    var n = data[i];
+    ctx.lineTo(x += sx, h * (n / m));
+  }
+  ctx.stroke();
+};
